ca436433b936b57db73ac95d56996b2548eff132
Ucat_OC <mc13650909668@163.com>
2026-05-01 20:39:44 +0800

    v0.2: add wild card priority, detect via WildCostType

--- mods/ComboClickMod/Plugin.cs
+++ mods/ComboClickMod/Plugin.cs
@@ -53,8 +53,10 @@ public class ComboClickBehaviour : MonoBehaviour
 
             var cards = handPile.GetComponentsInChildren<CardModel>(true);
 
-            CardModel bestCard = null;
-            int bestCost = int.MaxValue;
+            CardModel bestNormal = null;
+            int bestNormalCost = int.MaxValue;
+            CardModel bestWild = null;
+            int bestWildCost = int.MaxValue;
 
             foreach (var card in cards)
             {
@@ -64,24 +66,42 @@ public class ComboClickBehaviour : MonoBehaviour
                 if (!IsComboHighlighted(card)) continue;
 
                 var cost = config.manaCost;
-                if (cost < bestCost)
+                if (IsWildCard(card))
                 {
-                    bestCost = cost;
-                    bestCard = card;
+                    if (cost < bestWildCost)
+                    {
+                        bestWildCost = cost;
+                        bestWild = card;
+                    }
+                }
+                else
+                {
+                    if (cost < bestNormalCost)
+                    {
+                        bestNormalCost = cost;
+                        bestNormal = card;
+                    }
                 }
             }
 
+            var bestCard = bestNormal ?? bestWild;
             if (bestCard != null)
                 playerModel.TryPlayCard(bestCard, true);
         }
         catch { }
     }
 
-    private static Il2CppSystem.Reflection.FieldInfo _comboElementsField;
-    private static Il2CppSystem.Reflection.PropertyInfo _activeSelfProp;
-    private static Il2CppSystem.Reflection.MethodInfo _getEnumeratorMethod;
-    private static Il2CppSystem.Reflection.MethodInfo _moveNextMethod;
-    private static Il2CppSystem.Reflection.PropertyInfo _currentProp;
+    private static bool IsWildCard(CardModel card)
+    {
+        try
+        {
+            var costType = card.CardCostType;
+            if (costType == null) return false;
+            return costType.GetIl2CppType().FullName.Contains("WildCostType");
+        }
+        catch { }
+        return false;
+    }
 
     private static bool IsComboHighlighted(CardModel card)
     {
@@ -89,48 +109,55 @@ public class ComboClickBehaviour : MonoBehaviour
         {
             var cv = card.CardView;
             if (cv == null) return false;
-
-            if (_comboElementsField == null)
-            {
-                _comboElementsField = cv.GetIl2CppType().GetField("_comboElements",
-                    Il2CppSystem.Reflection.BindingFlags.Public |
-                    Il2CppSystem.Reflection.BindingFlags.NonPublic |
-                    Il2CppSystem.Reflection.BindingFlags.Instance);
-                if (_comboElementsField == null) return false;
-            }
-
-            var list = _comboElementsField.GetValue(cv);
-            if (list == null) return false;
-
-            if (_activeSelfProp == null)
-            {
-                var goType = Il2CppSystem.Type.GetType("UnityEngine.GameObject, UnityEngine.CoreModule", false);
-                _activeSelfProp = goType?.GetProperty("activeSelf");
-                if (_activeSelfProp == null) return false;
-            }
-
-            if (_getEnumeratorMethod == null)
-                _getEnumeratorMethod = list.GetIl2CppType().GetMethod("GetEnumerator");
-
-            var en = _getEnumeratorMethod.Invoke(list, null);
-            if (en == null) return false;
-            var et = en.GetIl2CppType();
-
-            if (_moveNextMethod == null)
-                _moveNextMethod = et.GetMethod("MoveNext");
-            if (_currentProp == null)
-                _currentProp = et.GetProperty("Current");
-
-            while (Unbox<bool>(_moveNextMethod.Invoke(en, null)))
-            {
-                var go = _currentProp.GetValue(en);
-                if (go == null) continue;
-                var av = _activeSelfProp.GetValue(go);
-                if (av != null && Unbox<bool>(av))
-                    return true;
-            }
+            return ListHasActive(cv, "_comboElements");
         }
         catch { }
+        return false;
+    }
+
+    private static Il2CppSystem.Reflection.PropertyInfo _activeSelfProp;
+    private static Il2CppSystem.Reflection.MethodInfo _getEnumeratorMethod;
+    private static Il2CppSystem.Reflection.MethodInfo _moveNextMethod;
+    private static Il2CppSystem.Reflection.PropertyInfo _currentProp;
+
+    private static bool ListHasActive(MonoBehaviour cv, string fieldName)
+    {
+        var field = cv.GetIl2CppType().GetField(fieldName,
+            Il2CppSystem.Reflection.BindingFlags.Public |
+            Il2CppSystem.Reflection.BindingFlags.NonPublic |
+            Il2CppSystem.Reflection.BindingFlags.Instance);
+        if (field == null) return false;
+
+        var list = field.GetValue(cv);
+        if (list == null) return false;
+
+        if (_activeSelfProp == null)
+        {
+            var goType = Il2CppSystem.Type.GetType("UnityEngine.GameObject, UnityEngine.CoreModule", false);
+            _activeSelfProp = goType?.GetProperty("activeSelf");
+            if (_activeSelfProp == null) return false;
+        }
+
+        if (_getEnumeratorMethod == null)
+            _getEnumeratorMethod = list.GetIl2CppType().GetMethod("GetEnumerator");
+
+        var en = _getEnumeratorMethod.Invoke(list, null);
+        if (en == null) return false;
+        var et = en.GetIl2CppType();
+
+        if (_moveNextMethod == null)
+            _moveNextMethod = et.GetMethod("MoveNext");
+        if (_currentProp == null)
+            _currentProp = et.GetProperty("Current");
+
+        while (Unbox<bool>(_moveNextMethod.Invoke(en, null)))
+        {
+            var go = _currentProp.GetValue(en);
+            if (go == null) continue;
+            var av = _activeSelfProp.GetValue(go);
+            if (av != null && Unbox<bool>(av))
+                return true;
+        }
         return false;
     }
 
